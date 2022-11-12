4acd972cfd5b258a481f46156a2de3a5b738b8e9
Jakub Brzozowski <brzozowski.jakub.andrzej@gmail.com>
2022-11-12 22:27:06 +0100

    Add User data model and DataStore logic

---
+++ SocialSiteWebApplication/SocialSiteWebApplication/DataStore/DataStore.cs
@@ -0,0 +1,134 @@
+using System.Collections.Concurrent;
+using System.ComponentModel;
+
+namespace SocialSiteWebApplication.DataStore;
+
+public class DataStore
+{
+    private static DataStore? _instance;
+    private static readonly object Lock = new object();
+    private List<User> _users = new List<User>();
+
+    private void Initialize()
+    {
+
+    }
+    
+    private DataStore()
+    {
+        Initialize();
+    }
+
+    public static DataStore Instance
+    {
+        get
+        {
+            lock(Lock)
+            {
+                return _instance ??= new DataStore();
+            }
+        }
+    }
+    
+    private User? GetUser(string userName)
+    {
+        return _users.FirstOrDefault(u => u.UserName == userName);
+
+    }
+    
+    public List<User> GetUsers()
+    {
+        return _users;
+    }
+    
+    public List<User> GetUsersFriends(string userName)
+    {
+        var user = GetUser(userName);
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+        
+        return user.Friends;
+    }
+
+    public void AddUser(string userName)
+    {
+        lock (Lock)
+        {
+            if (GetUser(userName) != null)
+            {
+                throw new Exception("User already exists");
+            }
+        
+            _users.Add(new User(userName));
+        }
+    }
+    
+    public void AddUserFriend(string userName, string friendUserName)
+    {
+        lock (Lock)
+        {
+            var user = GetUser(userName);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+        
+            var friend = GetUser(friendUserName);
+            if (friend == null)
+            {
+                throw new Exception("Friend not found");
+            }
+            
+            if (user.Friends.Contains(friend))
+            {
+                throw new Exception($"{friendUserName} is already a friend of {userName}");
+            }
+        
+            user.Friends.Add(friend);
+            friend.Friends.Add(user);
+        }
+    }
+    
+    public void RemoveUser(string userName)
+    {
+        lock (Lock)
+        {
+            var user = GetUser(userName);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+            
+            user.Friends.ForEach(f => f.Friends.Remove(user));
+            _users.Remove(user);
+        }
+    }
+    
+    public void RemoveUserFriend(string userName, string friendUserName)
+    {
+        lock (Lock)
+        {
+            var user = GetUser(userName);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+        
+            var friend = GetUser(friendUserName);
+            if (friend == null)
+            {
+                throw new Exception("Friend not found");
+            }
+
+            if (!user.Friends.Contains(friend))
+            {
+                throw new Exception($"{friendUserName} is not a friend of {userName}");
+            }
+            
+            user.Friends.Remove(friend);
+            friend.Friends.Remove(user);
+        }
+    }
+}
---
+++ SocialSiteWebApplication/SocialSiteWebApplication/DataStore/User.cs
@@ -0,0 +1,16 @@
+namespace SocialSiteWebApplication.DataStore;
+
+[Serializable]
+public class User
+{
+    public string UserName { get; set; }
+    public List<User> Friends { get; set; }
+    public DateTime CreatedDate { get; set; }
+    
+    public User(string userName)
+    {
+        UserName = userName;
+        Friends = new List<User>();
+        CreatedDate = DateTime.Now;
+    }
+}
