af94a5ba54b90204aa25cd56e0a07484f6632a9e
Jakub Brzozowski <brzozowski.jakub.andrzej@gmail.com>
2022-11-13 17:49:44 +0100

    Add custom error page

--- SocialSiteWebApplication/SocialSiteWebApplication/Models/ErrorViewModel.cs
+++ SocialSiteWebApplication/SocialSiteWebApplication/Models/ErrorViewModel.cs
@@ -3,6 +3,10 @@ namespace SocialSiteWebApplication.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
+    
+    public string? Message { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    
+    public bool ShowMessage => !string.IsNullOrEmpty(Message);
 }
