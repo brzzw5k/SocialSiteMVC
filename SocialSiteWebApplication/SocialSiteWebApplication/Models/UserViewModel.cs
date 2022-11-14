c5b925b7567b87fe7e0a01fee993443f156ed341
Jakub Brzozowski <brzozowski.jakub.andrzej@gmail.com>
2022-11-14 22:27:55 +0100

    Change user view

--- SocialSiteWebApplication/SocialSiteWebApplication/Models/UserViewModel.cs
+++ SocialSiteWebApplication/SocialSiteWebApplication/Models/UserViewModel.cs
@@ -5,13 +5,13 @@ namespace SocialSiteWebApplication.Models;
 public class UserViewModel
 {
     public string UserName { get; set; }
-    public string NewFriendUserName { get; set; }
+    public string FriendName { get; set; }
     public List<User> Friends { get; set; }
     
     public UserViewModel(string userName, List<User> friends)
     {
         UserName = userName;
-        NewFriendUserName = string.Empty;
+        FriendName = string.Empty;
         Friends = friends;
     }
 }
